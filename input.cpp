207717d21219c88488770068588687fc4661fa55
Schnoww <96844872+Matt-Wiszniewski@users.noreply.github.com>
2024-01-02 21:15:49 -0500

    Create input.cpp
    the input test file for the minifier

---
+++ input.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+/*test command              single              lines */                             
+/* multiline comment 
+         example */
+
+
+int input_main() {
+    // single-line comment
+
+    std::cout << "Hello, " /* inline comment */ << "World!\n";
+
+    /* Another multiline comment 
+         example */
+
+    return 0;
+}
